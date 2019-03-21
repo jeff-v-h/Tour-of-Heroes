6ffed4ff5c9c4421cd8335c329cff7708c197b09
DELTATRE\Jeff.Huang <jeff.huang@deltatre.it>
2019-03-21 16:25:03 +0000

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,22 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
   ],
   bootstrap: [ AppComponent ]
 })
